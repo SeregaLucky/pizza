29438e5c706d853b207c593b6d570c942d202805
SeregaLucky <strizgsergey@gmail.com>
2020-06-08 22:42:38 +0300

    add

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,13 +1,13 @@
-// import { combineReducers } from 'redux';
-// /* import - reducers */
-// import authReducer from './auth/authReducer';
-// import productReducer from './product/productReducer';
-// import globalReducer from './global/globalReducer';
+import { combineReducers } from 'redux';
+/* import - reducers */
+import authReducer from './auth/authReducer';
+import productReducer from './product/productReducer';
+import globalReducer from './global/globalReducer';
 
-// const rootReducer = combineReducers({
-//   auth: authReducer,
-//   products: productReducer,
-//   global: globalReducer,
-// });
+const rootReducer = combineReducers({
+  auth: authReducer,
+  products: productReducer,
+  global: globalReducer,
+});
 
-// export default rootReducer;
+export default rootReducer;
